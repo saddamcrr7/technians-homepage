de522b150394f57b0dad3f346e55862a7979e04b
Hossain Saddam <hossaincrr7@gmail.com>
2020-08-01 08:46:26 +0600

    update hero sliders

--- src/scripts/components/hero-slider.js
+++ src/scripts/components/hero-slider.js
@@ -3,9 +3,16 @@ import Swiper, {
   Pagination,
   Controller,
   EffectFade,
+  Autoplay
 } from 'swiper';
 // configure Swiper to use modules
-Swiper.use([Navigation, Pagination, Controller, EffectFade]);
+Swiper.use([Navigation, Pagination, Controller, EffectFade, Autoplay]);
+
+const nextsliderElm = document.querySelector('.c-next-slider')
+const imageSliderElm = document.querySelector('.c-iamges-slider')
+const projectSliderElm = document.querySelector('.c-project-slider')
+const progressLine = document.querySelector('.c-progress-slider__line')
+
 
 const heroNextSlider = new Swiper('.c-next-slider', {
   slidesPerView: 1,
@@ -22,6 +29,16 @@ const heroCountSlider = new Swiper('.c-count-slider', {
   wrapperClass: 'c-count-slider__wrapper',
   slideClass: 'c-count-slider__item',
   slideActiveClass: 'c-count-slider__item--active',
+  loop: true,
+  allowTouchMove: false
+})
+
+const heroProgressSlider = new Swiper('.c-progress-slider__container', {
+  direction: 'vertical',
+  slidesPerView: 1,
+  wrapperClass: 'c-progress-slider__wrapper',
+  slideClass: 'c-progress-slider__item',
+  slideActiveClass: 'c-progress-slider__item--active',
   loop: true,
   allowTouchMove: false
 })
@@ -45,9 +62,61 @@ const heroImagesSlider = new Swiper('.c-iamges-slider', {
   slideClass: 'c-iamges-slider__item',
   slideActiveClass: 'c-iamges-slider__item--active',
   loop: true,
+  autoplay: {
+    delay: 3000,
+  },
 
   controller: {
-    control: [heroNextSlider, heroCountSlider, heroProjectSlider],
+    control: [heroNextSlider, heroCountSlider, heroProjectSlider,
+      heroProgressSlider
+    ],
     by: 'container'
   },
+})
+
+heroImagesSlider.on('slideChange', (e) => {
+  progressLine.classList.remove('is-active')
+  if (!heroImagesSlider.autoplay.running) return
+  setTimeout(() => {
+    progressLine.classList.add('is-active')
+  }, 10)
+})
+
+
+window.addEventListener('load', (e) => {
+  progressLine.classList.add('is-active')
+})
+
+nextsliderElm.addEventListener('click', (e) => {
+  heroImagesSlider.slideNext()
+})
+
+imageSliderElm.addEventListener('mouseenter', () => {
+  heroImagesSlider.autoplay.stop()
+  progressLine.classList.remove('is-active')
+})
+
+imageSliderElm.addEventListener('mouseleave', () => {
+  heroImagesSlider.autoplay.start()
+  progressLine.classList.add('is-active')
+})
+
+nextsliderElm.addEventListener('mouseenter', () => {
+  heroImagesSlider.autoplay.stop()
+  progressLine.classList.remove('is-active')
+})
+
+nextsliderElm.addEventListener('mouseleave', () => {
+  heroImagesSlider.autoplay.start()
+  progressLine.classList.add('is-active')
+})
+
+projectSliderElm.addEventListener('mouseenter', () => {
+  heroImagesSlider.autoplay.stop()
+  progressLine.classList.remove('is-active')
+})
+
+projectSliderElm.addEventListener('mouseleave', () => {
+  heroImagesSlider.autoplay.start()
+  progressLine.classList.add('is-active')
 })

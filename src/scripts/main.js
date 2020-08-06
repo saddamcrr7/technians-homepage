90fa8a55f2500772aa43ea3bceaf71ce5dc51ccb
Hossain Saddam <hossaincrr7@gmail.com>
2020-08-07 00:17:40 +0600

    Update Featured Work & creative section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 import './components/navbar'
 import './components/hero-slider'
 import './components/testimonial-slider'
-import  './components/blogs-news-slider'
+import './components/blogs-news-slider'
 import './components/picks-slider'
 
 const creativeCard1 = document.querySelector('.o-creative-studio__card--1')
@@ -23,4 +23,34 @@ creativeCard3.addEventListener('mouseover', (e) => {
 creativeCard3.addEventListener('mouseout', (e) => {
   creativeCard1.style.transform = 'translateX(0%)'
   creativeCard2.style.transform = 'translateX(-70%)'
+})
+
+const creativeSction = document.querySelector('.o-creative-studio')
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > (creativeSction.offsetTop - window.innerHeight) + 200) {
+    creativeSction.classList.add('is-scrolled')
+  }else {
+    creativeSction.classList.remove('is-scrolled')
+  }
+})
+
+
+const featuredCompanys = document.querySelectorAll('.o-featured-work__company')
+const featuredImages= document.querySelectorAll('.o-featured-work__box-image')
+
+featuredCompanys.forEach((featuredCompany, i) => {
+  featuredCompany.addEventListener('mouseover',()=> {
+    featuredImages.forEach(featuredImage => {
+      featuredImage.style.opacity = '0'
+    })
+    featuredImages[i].style.opacity = '1'
+  })
+})
+
+
+const scrollDownBtn = document.querySelector('.o-hero__scroll')
+
+scrollDownBtn.addEventListener('click', ()=> {
+  window.scrollTo(0, window.innerHeight)
 })
